d26f7879211eb730647906c989d2d5018eaa41d1
eduque <emiliano.duque@endava.com>
2023-01-15 21:39:37 -0500

    agrege datos al programa de las diferentes capas

---
+++ Sol_PuntoVenta.Datos/Conexion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Sol_PuntoVenta.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.Base = "BD_PUNTOVENTA";
+            this.Servidor = "EN911749\\SQLEXPRESS";
+            this.Usuario = "user_sistema";
+            this.Clave = "soporte";
+
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor +
+                    "; DataBase=" + this.Base +
+                    "; User Id=" + this.Usuario +
+                    "; Password=" + this.Clave;
+
+            }
+            catch (Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if(Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
---
+++ Sol_PuntoVenta.Entidades/E_Punto_Venta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sol_PuntoVenta.Entidades
+{
+    public class E_Punto_Venta
+    {
+        public int Codigo_pv { get; set; }
+        public string Descripcion_pv { get; set; }
+
+    }
+}
